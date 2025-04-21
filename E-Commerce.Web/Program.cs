791731ce5e976ea17cc15098f39322f3454d7879
abdallh.mahfouz97@outlook.com <abdallh.mahfouz97@outlook.com>
2025-04-21 02:24:17 +0200

    Part03 is Done

--- E-Commerce.Web/Program.cs
+++ E-Commerce.Web/Program.cs
@@ -8,7 +8,7 @@ namespace E_Commerce.Web
 {
     public class Program
     {
-        public static async void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
