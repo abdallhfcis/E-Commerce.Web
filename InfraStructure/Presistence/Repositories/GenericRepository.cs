193bc31527a978bd3f21daa9d53fc2022a6f6539
abdallh.mahfouz97@outlook.com <abdallh.mahfouz97@outlook.com>
2025-04-22 02:29:01 +0200

    Part02 Done

--- InfraStructure/Presistence/Repositories/GenericRepository.cs
+++ InfraStructure/Presistence/Repositories/GenericRepository.cs
@@ -16,10 +16,25 @@ namespace Presistence.Repositories
 
         public async Task<IEnumerable<TEntity>> GetAllAsync() => await _dbContext.Set<TEntity>().ToListAsync();
 
+       
+
         public async Task<TEntity?> GetByIdAsync(TKey id) => await _dbContext.Set<TEntity>().FindAsync(id);
+
+       
 
         public void Remove(TEntity entity) => _dbContext.Remove(entity);
 
         public void Update(TEntity entity) => _dbContext.Update(entity);
+
+        #region With Specifications
+        public async Task<IEnumerable<TEntity>> GetAllAsync(ISpecifications<TEntity, TKey> specifications)
+        {
+            return await SpecificationEvaluator.CreateQuery(_dbContext.Set<TEntity>(), specifications).ToListAsync();
+        }
+        public async Task<TEntity?> GetByIdAsync(ISpecifications<TEntity, TKey> specifications)
+        {
+            return await SpecificationEvaluator.CreateQuery(_dbContext.Set<TEntity>(), specifications).FirstOrDefaultAsync();
+        }
+        #endregion
     }
 }
