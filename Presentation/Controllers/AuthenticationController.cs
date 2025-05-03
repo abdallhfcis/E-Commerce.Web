685ccf57503abb848f4a71cee86160902359b6bc
abdallh.mahfouz97@outlook.com <abdallh.mahfouz97@outlook.com>
2025-05-03 17:32:44 +0300

    Part06 Done

---
+++ Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ServiceAbstraction;
+using Shared.Identity;
+
+namespace Presentation.Controllers
+{
+    public class AuthenticationController(IServiceManager _serviceManager):ApiBaseController
+    {
+        //Login
+        [HttpPost("Login")]
+        public async Task<ActionResult<UserDto>> Login (LoginDto loginDto)
+        {
+            var User= await _serviceManager.AuthenticationService.LgainAsync(loginDto);
+            return Ok(User);
+        }
+        [HttpPost("Register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto rigsterDto)
+        {
+            var User = await _serviceManager.AuthenticationService.RegisterAsync(rigsterDto);
+            return Ok(User);
+        }
+    }
+}
