fe8e8e3075b7481eaa84b6ce16193ab9723c6ff5
abdallh.mahfouz97@outlook.com <abdallh.mahfouz97@outlook.com>
2025-05-13 16:59:50 +0300

    Part 010 Done

--- Presentation/Controllers/AuthenticationController.cs
+++ Presentation/Controllers/AuthenticationController.cs
@@ -1,21 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ServiceAbstraction;
 using Shared.Identity;
 
 namespace Presentation.Controllers
 {
-    public class AuthenticationController(IServiceManager _serviceManager):ApiBaseController
+    public class AuthenticationController(IServiceManager _serviceManager) : ApiBaseController
     {
         //Login
         [HttpPost("Login")]
-        public async Task<ActionResult<UserDto>> Login (LoginDto loginDto)
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var User= await _serviceManager.AuthenticationService.LgainAsync(loginDto);
+            var User = await _serviceManager.AuthenticationService.LgainAsync(loginDto);
             return Ok(User);
         }
         [HttpPost("Register")]
@@ -23,6 +25,42 @@ namespace Presentation.Controllers
         {
             var User = await _serviceManager.AuthenticationService.RegisterAsync(rigsterDto);
             return Ok(User);
+        }
+
+        //check Email
+        [HttpGet]
+        public async Task<ActionResult<bool>> CheckEmail(string email)
+        {
+            var Email = await _serviceManager.AuthenticationService.CheckEmailAsync(email);
+            return Ok(Email);
+        }
+        //Get Current User 
+        [Authorize]
+        [HttpGet("CurrentUser")]
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
+        {
+            var Email = User.FindFirstValue(ClaimTypes.Email);
+            var AppUser = await _serviceManager.AuthenticationService.GetCurrentUserAsync(Email);
+            return Ok(AppUser);
+        }
+        //Get Current user Address
+        [Authorize]
+        [HttpGet("Address")]
+        public async Task<ActionResult<AddressDto>> GetCurrentUserAddress()
+        {
+            var Email = User.FindFirstValue(ClaimTypes.Email);
+            var AppUser = await _serviceManager.AuthenticationService.GetCurrentUserAddressAsync(Email);
+            return Ok(AppUser);
+        }
+        //Update Current user Address
+        [Authorize]
+        [HttpPut("Address")]
+        public async Task<ActionResult<AddressDto>> UpdateCurrentUserAddress(AddressDto addressDto)
+        {
+            var Email = User.FindFirstValue(ClaimTypes.Email);
+            var UpdatedAddress = await _serviceManager.AuthenticationService.UpdateCurrentUserAddressAsync(Email, addressDto);
+            return Ok(UpdatedAddress);
+
         }
     }
 }
